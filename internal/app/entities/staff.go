395599461cf3778da4da2d7d676ce7221b3fbfe4
Meles-Z <meles.zawdie@gmail.com>
2025-01-23 15:59:16 +0300

    staff entities

--- internal/app/entities/staff.go
+++ internal/app/entities/staff.go
@@ -1,17 +1,21 @@
 package entities
 
-import "time"
+import (
+	"time"
+
+	"github.com/shopspring/decimal"
+)
 
 type Staff struct {
 	Model
-	FirstName   string
-	LastName    string
-	Email       string
-	PhoneNumber string
-	Role        string
-	Adress      string
-	Department  Department
-	Salary      string
-	DateOfJoin  time.Time
-	Status      string
+	FirstName   string          `json:"firstName"`
+	LastName    string          `json:"lastName"`
+	Email       string          `json:"email"`
+	PhoneNumber string          `json:"phoneNumber"`
+	Role        string          `json:"role"`
+	Address     string          `json:"address"`
+	Salary      decimal.Decimal `json:"salary"`
+	DateOfJoin  time.Time       `json:"dateOfJoin"`
+	Status      string          `json:"status"`
+	Department  []Department    `json:"department"`
 }
